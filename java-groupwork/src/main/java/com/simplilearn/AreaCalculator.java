ff20b66eebdfde007907def26b99c56f26c981c8
Ariunchimeg Sanchir <asanchir1@gmail.com>
2023-09-29 20:52:17 -0700

    Finished AreaCalculator

--- java-groupwork/src/main/java/com/simplilearn/AreaCalculator.java
+++ java-groupwork/src/main/java/com/simplilearn/AreaCalculator.java
@@ -22,6 +22,75 @@ public class AreaCalculator {
 
 	}
 
+	private int areaRectangle() {
+
+		shapes();
+		Scanner num = new Scanner(System.in);
+
+		System.out.println("Which Shape : \n");
+		int shape = num.nextInt();
+
+		System.out.println("Side Length : \n");
+		int length = num.nextInt();
+
+		System.out.println("Side Width : \n");
+		int width = num.nextInt();
+
+		int area = length * width;
+		System.out.println("The Area is: " + area + "\n");
+
+		return shape;
+
+	}
+
+	private int areaTriangle() {
+		shapes();
+		Scanner num = new Scanner(System.in);
+
+		System.out.println("Which Shape : \n");
+		int shape = num.nextInt();
+
+		System.out.println("Side Base : \n");
+		int base = num.nextInt();
+		
+		System.out.println("Side Height : \n");
+		int height = num.nextInt();
+
+		System.out.println("The Area is: " + 0.5 * base * height + "\n");
+
+		return shape;
+
+	}
+	
+	private int areaCircle() {
+
+		shapes();
+		Scanner num = new Scanner(System.in);
+
+		System.out.println("Which Shape : \n");
+		int shape = num.nextInt();
+
+		System.out.println("Side Radius : \n");
+		int radius = num.nextInt();
+
+		double area = Math.PI * radius * radius;
+		System.out.println("The Area is: " + area + "\n");
+
+		return shape;
+
+	}
+	
+	private static void quit() {
+		shapes();
+		Scanner num = new Scanner(System.in);
+		
+		System.out.println("Which Shape : \n");
+		int shape = num.nextInt();
+
+		System.out.println("Invalid Shape");
+		
+	}
+
 	private static void shapes() {
 
 		System.out.println("Shape Area Calculator \n");
@@ -38,7 +107,11 @@ public class AreaCalculator {
 
 		AreaCalculator result = new AreaCalculator();
 
-		result.areaSquare();
+		// result.areaSquare();
+		// result.areaRectangle();
+		// result.areaTriangle();
+		// result.areaCircle();
+		result.quit();
 
 	}
 
