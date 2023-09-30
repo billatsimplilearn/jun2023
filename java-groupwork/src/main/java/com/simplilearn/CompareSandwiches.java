abd55160700ff243d4a5c7a48bdffe8550e889e9
hectordelgadillo <romanhd93@gmail.com>
2023-09-30 13:04:42 -0500

    Sandwhiches

---
+++ java-groupwork/src/main/java/com/simplilearn/CompareSandwiches.java
@@ -0,0 +1,36 @@
+package com.simplilearn;
+
+import java.util.Arrays;
+
+public class CompareSandwiches {
+	
+	public boolean sandwich (String [] sandwhich1, String [] sandwhich2) {
+		
+		
+		
+		Object lenght;
+		Object length;
+		if ( sandwhich1.equals(length != sandwhich2. lenght) {
+			return false;
+		
+		}
+		Arrays.sort(sandwhich1);
+		Arrays.sort(sandwhich2);
+		
+		Arrays.equals(sandwhich1, sandwhich2);
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		
+		
+		
+		
+	}
+
+}
