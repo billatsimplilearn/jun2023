7a9f8d3f87e224579812f67a702db7426ef37e8d
Huseyin Erikli <herikli33@gmail.com>
2023-10-01 10:04:39 -0400

    commiting sandwiches to our branch, team2

--- java-groupwork/src/main/java/com/simplilearn/CompareSandwiches.java
+++ java-groupwork/src/main/java/com/simplilearn/CompareSandwiches.java
@@ -1,36 +1,43 @@
 package com.simplilearn;
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CompareSandwiches {
-	
-	public boolean sandwich (String [] sandwhich1, String [] sandwhich2) {
-		
-		
-		
-		Object lenght;
-		Object length;
-		if ( sandwhich1.equals(length != sandwhich2. lenght) {
-			return false;
-		
+	public class CompareSandwiches {
+
+		public boolean hasSameBread(List<String> sand1, List<String> sand2) {
+			return sand1.get(0).equals(sand2.get(0)) && sand1.get(sand1.size() - 1).equals(sand2.get(sand1.size() - 1));
 		}
-		Arrays.sort(sandwhich1);
-		Arrays.sort(sandwhich2);
-		
-		Arrays.equals(sandwhich1, sandwhich2);
+
+		public static void main(String[] args) {
+
+			List<String> sandwich1 = new ArrayList<String>();
+			List<String> sandwich2 = new ArrayList<String>();
+			List<String> sandwich3 = new ArrayList<String>();
+			List<String> sandwich4 = new ArrayList<String>();
+			List<String> sandwich5 = new ArrayList<String>();
+			List<String> sandwich6 = new ArrayList<String>();
+
+			sandwich1.addAll(List.of("white bread", "lettuce", "cheese", "turkey", "white bread"));
+			sandwich2.addAll(List.of("white bread", "lettuce", "cheese", "turkey", "white bread"));
+
+			sandwich3.addAll(List.of("white bread", "lettuce", "tomato", "brown bread"));
+			sandwich4.addAll(List.of("white bread", "lettuce", "tomato", "white bread"));
+
+			sandwich5.addAll(List.of("rye bread", "lettuce", "cheese", "ham", "white bread"));
+			sandwich6.addAll(List.of("white bread", "lettuce", "cheese", "ham", "rye bread"));
+
+			CompareSandwiches res = new CompareSandwiches();
+
+			boolean resPair1 = res.hasSameBread(sandwich1, sandwich2);
+			boolean resPair2 = res.hasSameBread(sandwich3, sandwich4);
+			boolean resPair3 = res.hasSameBread(sandwich5, sandwich6);
+
+			System.out.println("first pair is: " + resPair1);
+			System.out.println("second pair is: " + resPair2);
+			System.out.println("third pair is: " + resPair3);
+
+		}
+
 	}
-	
-	
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	
-		
-		
-		
-		
-	}
-
-}
