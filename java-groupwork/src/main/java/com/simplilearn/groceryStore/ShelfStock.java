7978c26786701b0bb551edb5ebf6ddb2f14d7e70
Huseyin Erikli <herikli33@gmail.com>
2023-10-10 20:38:47 -0400

    pushing grocery store, after shelfstock

---
+++ java-groupwork/src/main/java/com/simplilearn/groceryStore/ShelfStock.java
@@ -0,0 +1,19 @@
+package com.simplilearn.groceryStore;
+
+public class ShelfStock {
+	private String name;
+	private int quantity;
+	private Department department;
+	private Category category;
+
+	public ShelfStock() {
+
+	}
+
+	public ShelfStock(String name) {
+		this.name = name;
+	}
+
+	
+
+}
